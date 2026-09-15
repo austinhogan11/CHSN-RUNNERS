679031ee1c7bbf130536ee3dc53d107567629ebf
Austin Hogan <austinhogan15@gmail.com>
2026-09-15 15:38:19 -0400

    fix actiond dprecation node 20

--- infra/bootstrap/github_oidc.tf
+++ infra/bootstrap/github_oidc.tf
@@ -34,7 +34,7 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
-        "repo:austinhogan11/CHSN-RUNNERS:*",
+        "repo:austinhogan11@60478173/CHSN-RUNNERS@1367812655:*",
       ]
     }
   }
