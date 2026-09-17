a611239824b8edd95b2b905bc08452cbebdaa35e
Austin Hogan <austinhogan15@gmail.com>
2026-09-17 18:36:53 -0400

    feat(infra): add API container registry

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -5,3 +5,7 @@ output "cloudfront_distribution_id" {
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.web.domain_name
 }
+
+output "api_ecr_repository_url" {
+  value = aws_ecr_repository.api.repository_url
+}
